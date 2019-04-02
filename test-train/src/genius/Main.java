9e6127267515813dfab5c874b4a6796562772a98
Dmitry <Drakon7719@gmail.com>
2019-04-02 18:22:16 +0400

    feat(*): Добавил ввод 10 чисел в массив и вывод большего из них

--- test-train/src/genius/Main.java
+++ test-train/src/genius/Main.java
@@ -1,24 +1,33 @@
 package genius;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
         int firstNumber = scan.nextInt();
         int secondNumber = scan.nextInt();
+        int thirdNumber = scan.nextInt();
+        int fourthNumber = scan.nextInt();
+        int fifthNumber = scan.nextInt();
+        int sixthNumber = scan.nextInt();
+        int seventhNumber = scan.nextInt();
+        int eighthNumber = scan.nextInt();
+        int ninthNumber = scan.nextInt();
+        int tenthNumber = scan.nextInt();
 
-        int result = firstNumber + secondNumber;
+        int[] mass = {firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber, seventhNumber, eighthNumber, ninthNumber, tenthNumber};
 
-        System.out.println("result = " + result);
+        int max = Integer.MIN_VALUE;
+        for(int i = 1; i < mass.length; i++){
+            max = Math.max(max, mass[i]);
+        }
+        System.out.println("Наибольшee число: " + max );
 
 
 
     }
-
-
-
-
 }
