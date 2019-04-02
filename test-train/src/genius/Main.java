5e48e748c0dba976f0c64dd986a27cc03624cf13
Dmitry <Drakon7719@gmail.com>
2019-04-02 20:41:14 +0400

    feat(*): Добавил заполнение массива в цикле

--- test-train/src/genius/Main.java
+++ test-train/src/genius/Main.java
@@ -8,21 +8,10 @@ public class Main {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
-        int firstNumber = scan.nextInt();
-        int secondNumber = scan.nextInt();
-        int thirdNumber = scan.nextInt();
-        int fourthNumber = scan.nextInt();
-        int fifthNumber = scan.nextInt();
-        int sixthNumber = scan.nextInt();
-        int seventhNumber = scan.nextInt();
-        int eighthNumber = scan.nextInt();
-        int ninthNumber = scan.nextInt();
-        int tenthNumber = scan.nextInt();
-
-        int[] mass = {firstNumber, secondNumber, thirdNumber, fourthNumber, fifthNumber, sixthNumber, seventhNumber, eighthNumber, ninthNumber, tenthNumber};
-
+        int[] mass = new int[10];
         int max = Integer.MIN_VALUE;
-        for(int i = 1; i < mass.length; i++){
+        for(int i = 0; i < mass.length; i++){
+            mass [i] = scan.nextInt();
             max = Math.max(max, mass[i]);
         }
         System.out.println("Наибольшee число: " + max );
